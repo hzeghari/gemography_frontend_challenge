9da3c5cb950a64bb703676d6ec91da40bfdea81c
Hamza Zeghari <zeghari.hamzaa@gmail.com>
2020-12-25 23:43:18 +0100

    Add Star and Issue buttons

--- src/components/FetchRepos.js
+++ src/components/FetchRepos.js
@@ -5,14 +5,32 @@ import React from 'react';
 import { Pagination } from 'antd';
 import 'antd/dist/antd.css';
 
+import GitHubButton from 'react-github-btn';
+
 
 export default class FetchRepos extends React.Component{
 
-    state={
+    constructor(props){
+      super(props)
+
+      var today = new Date(),
+      date = today.getFullYear() + '-' + (today.getMonth()+1) + '-' + today.getDate();
+      
+      this.state={
         repos: [],
         loading: true,
         page: 1,
-    };
+        currentDate: date,
+      };
+      console.log(date);
+
+      var msDiff = new Date("June 30, 2035").getTime() - new Date().getTime();    //Future date - current date
+      var daysTill30June2035 = Math.floor(msDiff / (1000 * 60 * 60 * 24));
+      console.log(daysTill30June2035);
+
+    }
+    
+    
 
     async componentDidMount(){
         var url="";
@@ -30,7 +48,7 @@ export default class FetchRepos extends React.Component{
             .catch(()=>{console.log("Api don't work at this time")})
     }
 
-    onChange = (page,pageSize) =>{ //si on change la page, l'api va changer aussi dans la page correspondante
+    onChange = (page,pageSize) =>{ // this is for change page ( Pagination )
     var url="";
     url="https://api.github.com/search/repositories?q=created:>2020-07-24&sort=stars&order=desc&page="+page+"&per_page"+pageSize;
     fetch(url, {
@@ -46,10 +64,15 @@ export default class FetchRepos extends React.Component{
         .catch(()=>{console.log("il ya un erreur dans la connexion avec le serveur de Github")})
     }
 
+    // getDifdays(){
+    //   var diff = 
+    //   return diff;
+    // }
 
     render(){
-
+      
     return (
+      
     <React.Fragment>
       <div class="py-12 px-4 text-center">
         <h2 class="text-4xl mb-2 leading-tight font-semibold font-heading">Gemography Front-End Challenge by <a class="underline" href="https://github.com/hzeghari">Hzeghari</a></h2>
@@ -62,7 +85,7 @@ export default class FetchRepos extends React.Component{
       <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg"></div> */}
 
         <Pagination defaultCurrent={1} pageSize={60} onChange={this.onChange} total={1000} />
-
+      
       <table class="w-screen table-fixed">
         <thead class='align-middle'>
           <tr>
@@ -86,15 +109,25 @@ export default class FetchRepos extends React.Component{
                         {element.description}
                     </div>
                     <div class="text-sm text-gray-500">
-                      <div className='mr-1 inline box-content h-16 w-16 border-2 border-purple-500 hover:border-gray-500'>
+                      {/* <div className='mr-1 inline box-content h-16 w-16 border-2 border-purple-500 hover:border-gray-500'>
                         Starts : {element.stargazers_count}
+                      </div> */}
+                      <div className='mr1 inline'>
+                        <GitHubButton href={element.html_url} data-icon="octicon-star" data-size="large" data-show-count="true" aria-label="Star ntkme/github-buttons on GitHub">
+                          Star
+                        </GitHubButton>
                       </div>
-                      <div className='mr-1 inline box-content h-16 w-16 border-2 border-purple-500 hover:border-gray-500'>
+                      {/* <div className='mr-1 inline box-content h-16 w-16 border-2 border-purple-500 hover:border-gray-500'>
                         Issues : {element.open_issues_count}
+                      </div> */}
+                      <div className='mr-1 inline'>
+                        <GitHubButton href={element.html_url} data-icon="octicon-issue-opened" data-size="large" data-show-count="true" aria-label="Issue ntkme/github-buttons on GitHub">
+                          Issue
+                        </GitHubButton>
                       </div>
-                      <div className='inline'>
-                        Created at {element.created_at.split("T0")[0]} by  {element.owner.login}
-                      </div>
+                      {/* <div className='inline'>
+                        Created at / {element.created_at} / {this.state.currentDate} by  {element.owner.login}
+                      </div> */}
                     </div>
                   </div>
                 </div>
